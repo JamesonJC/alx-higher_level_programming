6abe0044a11171bc17e59e21e55755eb12ae6f16
JamesonJc <jamesonpillay@gmail.com>
2023-08-16 23:47:39 +0000

    1. Search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    if my_list:
+        for idx in my_list:
+            if idx == my_list[search]:
+                my_list[elemen] = replace
+    return (my_list)
