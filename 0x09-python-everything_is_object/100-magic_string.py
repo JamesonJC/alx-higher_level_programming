09dac9975e8224de7156dcb8741165b97747b93f
JamesonJc <jamesonpillay@gmail.com>
2023-09-05 21:30:22 +0000

    29. #pythonic

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+class MagicString:
+    def __init__(self):
+        self.n = 0
+
+    def __call__(self):
+        self.n += 1
+        return ("BestSchool, " * (self.n - 1) + "BestSchool")
+
+
+magic_string = MagicString()
