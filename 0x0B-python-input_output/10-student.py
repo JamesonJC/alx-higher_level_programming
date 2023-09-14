5bf9b9210e4ed32210e9c5e69079afd8c7e9a440
Musa Ketani <Musa.ketani@sp-vip.co.za>
2023-09-14 10:41:35 +0200

    10. Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+''' A class Student defination. '''
+
+
+class Student:
+    '''Class represantation of a student.'''
+
+    def __init__(self, first_name, last_name, age):
+        '''
+        Constructer for Student class.
+
+        Args:
+            first_name (str): Student first name.
+            last_name (str): Student last name.
+            age (int): Student age.
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''
+        Defines student in the form of a dictionary.
+
+        Args:
+            attrs (list): The attriboutes of the the class.
+        '''
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
