909ec97d7deba1a850957cc3c521c5d048af5e61
Musa Ketani <Musa.ketani@sp-vip.co.za>
2023-09-14 13:30:45 +0200

    1. My list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+''' MyList class representation '''
+
+
+class MyLIst(list):
+    ''' A defination of class MyList '''
+
+    def print_sorted(self):
+        ''' Print sorted elements of the list '''
+
+        sorted_list = sorted(self)
+        print(sorted_lis)
