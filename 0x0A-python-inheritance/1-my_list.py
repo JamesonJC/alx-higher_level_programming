5e137f0a4f707774fe66b48c6882c140f08457ce
Musa Ketani <Musa.ketani@sp-vip.co.za>
2023-09-14 13:45:06 +0200

    1. My list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,13 +1,33 @@
 #!/usr/bin/python3
 
-''' MyList class representation '''
+''' MyList class representation. '''
 
 
 class MyLIst(list):
-    ''' A defination of class MyList '''
+    ''' 
+    A subclass of class MyList with added method for printing the list.
+
+    Attributes:
+        No attributes added.
+
+    Methods:
+        print_sorted():
+            print the list arranged/or sorted.
+
+
+    '''
+    def __int__(self):
+        ''' Constructor for an empty Mylist Object. '''
+        super().__init__()
 
     def print_sorted(self):
-        ''' Print sorted elements of the list '''
+        '''
+        Print sorted elements of the list.
+        Args:
+            None.
+        Returns:
+            None.
 
+        '''
         sorted_list = sorted(self)
         print(sorted_lis)
