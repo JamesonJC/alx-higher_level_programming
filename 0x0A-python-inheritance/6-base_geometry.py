4a74e8d952a7c5b43f8bf751cadb038283c13b6b
Musa Ketani <Musa.ketani@sp-vip.co.za>
2023-09-15 11:32:10 +0200

    6. Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Empty class BaseGeometry representation.'''
+
+
+class BaseGeometry:
+    '''Defination of base geometry.'''
+
+    def area(self):
+        '''Defination of area, not implemented'''
+        raise Exception("area() is not implemented")
