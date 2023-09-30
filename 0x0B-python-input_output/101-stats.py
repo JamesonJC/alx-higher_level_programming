0563571f4a50fdf82cfebc550bb660173169194d
JamesonJc <jamesonpillay@gmail.com>
2023-09-30 17:25:09 +0000

    14. Log parsing

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,36 +1,39 @@
 #!/usr/bin/python3
+'''Log parsing script Modules'''
 import sys
 
 
-def print_status():
-    ''' Function print the status request '''
-    c = 0
-    s = 0
-    f_s = 0
-    s_c = {"200": 0, "301": 0, "400": 0, "401": 0,
-                    "403": 0, "404": 0, "405": 0, "500": 0}
-
-    for l in sys.stdin:
-        line = l.split()
-        try:
-            s += int(line[-1])
-            c = line[-2]
-            s_c[code] += 1
-        except:
-            continue
-        if c == 9:
-            print("File size: {}".format(s))
-            for key, val in sorted(s_c.items()):
-                if (val != 0):
-                    print("{}: {}".format(key, val))
-            c = 0
-        c += 1
-    if c < 9:
-        print("File size: {}".format(s))
-        for key, val in sorted(s_c.items()):
-            if (val != 0):
-                print("{}: {}".format(key, val))
-
-
 if __name__ == "__main__":
-    print_status()
+    size = [0]
+    codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+
+    def check_match(line):
+        '''Checks for regexp match in line.'''
+        try:
+            line = line[:-1]
+            words = line.split(" ")
+            size[0] += int(words[-1])
+            code = int(words[-2])
+            if code in codes:
+                codes[code] += 1
+        except ValueError:
+            pass
+
+    def print_stats():
+        '''Prints accumulated statistics.'''
+        print("File size: {}".format(size[0]))
+        for k in sorted(codes.keys()):
+            if codes[k]:
+                print("{}: {}".format(k, codes[k]))
+
+    i = 1
+    try:
+        for line in sys.stdin:
+            check_match(line)
+            if i % 10 == 0:
+                print_stats()
+            i += 1
+    except KeyboardInterrupt:
+        print_stats()
+        raise
+    print_stats()
